4c86537711ca2722a97fd97a825c7736c7a1896b
Sam Oluoch <samsoluoch@gmail.com>
2018-09-26 15:12:49 +0300

    created a Categories model, and migrated the database

--- photos/models.py
+++ photos/models.py
@@ -5,3 +5,9 @@ class Location(models.Model):
     district = models.CharField(max_length =30)
     county = models.CharField(max_length =30)
     country = models.CharField(max_length=30)
+
+class Category(models.Model):
+    name = models.CharField( max_length=100 )
+    parent = models.ForeignKey('self', null=True, blank=True, related_name='children')
+    slug = models.SlugField(unique=True)
+    # mptt.register(Category)
