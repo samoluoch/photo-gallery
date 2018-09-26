8499f72aae401a2b2fb9f190ac51a50f46f0ae52
Sam Oluoch <samsoluoch@gmail.com>
2018-09-26 10:39:48 +0300

    initial commit

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/MacbookAir/Desktop/MORINGA/CORE/PYTHON/DJANGO/Gallery/virtual/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
