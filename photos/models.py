f7f8a9dbdf5eb81e4316fe2e862e4f77743e4c4a
Sam Oluoch <samsoluoch@gmail.com>
2018-09-26 15:04:50 +0300

    created the Location Model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Location(models.Model):
+    district = models.CharField(max_length =30)
+    county = models.CharField(max_length =30)
+    country = models.CharField(max_length=30)
